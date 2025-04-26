91cb5885edd516fc831b68f93016138a2f691779
Bharti Sharma <bhartisharma@Bhartis-MacBook-Air.local>
2025-04-26 07:21:47 +0530

    func

--- deck.go
+++ deck.go
@@ -6,8 +6,30 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardTypes := []string{"Spades", "Clubs", "Diamonds", "Hearts"}
+	cardNumber := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, eachCardcardType := range cardTypes {
+		for _, eachCardNumber := range cardNumber {
+			cards = append(cards, eachCardNumber+" of "+eachCardcardType)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for index, card := range d {
 		fmt.Println(index, card)
 	}
+}
+
+func deal(d deck, handSize int) []deck {
+	// fmt.Println(d, handSize)
+	decks := make([]deck, 2)
+	decks[0] = d[:handSize]
+	decks[1] = d[handSize:]
+
+	return decks
 }
--- main.go
+++ main.go
@@ -5,27 +5,23 @@ import "fmt"
 func main() {
 	// var card string = "Ace of Spades"
 	card := "Ace of Spades"
-	card = newCard("Two")
+	// card = newCard("Two")
 	fmt.Println(card)
 
-	cards := deck{newCard("Five"), newCard("One"), newCard("Three"), "April fool bnaya"}
-	cards = append(cards, newCard("No cards"), "Testing")
-
+	// cards := deck{newCard("Five"), newCard("One"), newCard("Three"), "April fool bnaya"}
+	cards := newDeck()
+	cards = append(cards, "Testing")
 	// iterating through the cards
-
 	/* Laymen way
 	for index, eachCard := range cards {
 		fmt.Println(index, eachCard)
 	}
 	*/
-
 	// from deck.go
-	cards.print()
+	// cards.print()
+
+	decks := deal(cards, 17)
+	fmt.Println(decks)
 
 	// fmt.Println(cards)
-
-}
-
-func newCard(cnt string) string {
-	return cnt + " of Daimonds"
 }
